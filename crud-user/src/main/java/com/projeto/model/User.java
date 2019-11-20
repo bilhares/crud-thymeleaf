513932f3e1acaa5ec90c4dcee79af6c5df6a2434
Felipe Bilhares Salles <05721985151@detran.ms.gov.br>
2019-11-20 14:10:13 -0300

    projeto

---
+++ crud-user/src/main/java/com/projeto/model/User.java
@@ -0,0 +1,46 @@
+package com.projeto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Name is mandatory")
+	private String name;
+	@NotBlank(message = "Email is mandatory")
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
